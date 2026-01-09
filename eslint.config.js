10c6c4cc146e05a12859e33ba8135918b102ea98
Ikuomola Stephen <cstephenid@yahoo.com>
2026-01-09 09:29:12 +0100

    style(core):use proper code  syntax in eslint file

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import js from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import jsdoc from 'eslint-plugin-jsdoc';
 import eslintPluginJsonc from 'eslint-plugin-jsonc';
-import perfectionist from 'eslint-plugin-perfectionist'
+import perfectionist from 'eslint-plugin-perfectionist';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
